bfde9d6a275ecc25f830143a49aab3626615575b
theMIslam <valvekakaxa@gmail.com>
2023-08-29 12:32:48 +0600

    first commit

---
+++ app/src/main/java/com/london/rickandmorty/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.london.rickandmorty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import retrofit2.Retrofit
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl()
+    }
+}
