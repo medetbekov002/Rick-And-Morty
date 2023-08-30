6cdb56bfe9b48e221de941d397a5f78f8dbc34cd
theMIslam <valvekakaxa@gmail.com>
2023-08-30 12:50:13 +0600

    added library retrofit, moshi, binding + added interface class service

--- app/src/main/java/com/london/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/london/rickandmorty/MainActivity.kt
@@ -2,14 +2,38 @@ package com.london.rickandmorty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
         val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl()
+            .baseUrl("https://rickandmortyapi.com/api/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+
+        val rickAndMortyService: RickAndMortyService =
+            retrofit.create(RickAndMortyService::class.java)
+
+        rickAndMortyService.getCharacterById().enqueue(object : Callback<Any> {
+            override fun onResponse(call: Call<Any>, response: Response<Any>) {
+                Log.i("MainActivity", response.toString())
+            }
+
+            override fun onFailure(call: Call<Any>, t: Throwable) {
+                Log.i("MainActivity", t.message ?: "Null message")
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/london/rickandmorty/RickAndMortyService.kt
@@ -0,0 +1,1 @@
+package com.london.rickandmortyimport retrofit2.Callimport retrofit2.http.GETinterface RickAndMortyService {    @GET("character/2")     fun getCharacterById(): Call<Any>}
